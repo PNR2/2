1160511dd8cbe3beed6a5ac8a4a7da501609f3c9
PNR2 <94699808+PNR2@users.noreply.github.com>
2026-08-29 13:19:40 +0530

    Update DiscoverySyncer.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/discovery/DiscoverySyncer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/discovery/DiscoverySyncer.kt
@@ -2,12 +2,8 @@
 
 package eu.kanade.tachiyomi.data.discovery
 
-import eu.kanade.tachiyomi.source.CatalogueSource
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withTimeoutOrNull
-import tachiyomi.domain.source.service.SourceManager
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
 object DiscoverySyncer {
     private val rssFetcher = RssNewsFetcher()
@@ -18,94 +14,34 @@ object DiscoverySyncer {
     suspend fun syncNow() {
         if (DiscoveryProgressState.progress.value.isRunning) return
 
-        DiscoveryProgressState.update(
-            isRunning = true,
-            percentage = 10,
-            message = "Starting sync...",
-        )
+        DiscoveryProgressState.update(true, 10, "Starting sync...")
 
+        // News
         try {
-            DiscoveryProgressState.update(
-                isRunning = true,
-                percentage = 25,
-                message = "Fetching latest anime news...",
-            )
-            withTimeoutOrNull(10000) {
-                val newsUrl = "https://www.animenewsnetwork.com/news/rss.xml"
-                val fetchedNews = rssFetcher.fetchNews(newsUrl, "Anime News Network")
-                if (fetchedNews.isNotEmpty()) {
-                    rssRepository.insertNews(fetchedNews)
-                }
+            DiscoveryProgressState.update(true, 30, "Fetching news...")
+            withTimeoutOrNull(12000) {
+                val news = rssFetcher.fetchNews(
+                    "https://www.animenewsnetwork.com/news/rss.xml",
+                    "Anime News Network",
+                )
+                if (news.isNotEmpty()) rssRepository.insertNews(news)
             }
-        } catch (e: Exception) {
-            // Skip
-        }
+        } catch (_: Exception) {}
 
+        // Seasonal
         try {
-            DiscoveryProgressState.update(
-                isRunning = true,
-                percentage = 50,
-                message = "Fetching seasonal manga from MAL...",
-            )
-            withTimeoutOrNull(20000) {
-                val fetchedManga = malFetcher.fetchSeasonalManga()
+            DiscoveryProgressState.update(true, 60, "Fetching seasonal manga...")
+            val mangaList = withTimeoutOrNull(20000) {
+                malFetcher.fetchSeasonalManga()
+            } ?: emptyList()
 
-                if (fetchedManga.isNotEmpty()) {
-                    DiscoveryProgressState.update(
-                        isRunning = true,
-                        percentage = 75,
-                        message = "Checking extension matching...",
-                    )
-                    val sourceManager: SourceManager = Injekt.get()
-                    val installedSources = sourceManager.getOnlineSources().filterIsInstance<CatalogueSource>()
-                    val activeSource = installedSources.firstOrNull()
-                    val isAutoOn = MalDiscoveryRepository.isAutomationEnabled()
+            DiscoveryProgressState.update(true, 85, "Saving to database...")
+            // Always try to save (even the test item)
+            malRepository.insertSeasonalManga(mangaList)
+        } catch (_: Exception) {}
 
-                    val matchedMangaList = fetchedManga.map { manga ->
-                        var matchedSourceId: Long? = null
-                        var matchedMangaUrl: String? = null
-
-                        if (isAutoOn && activeSource != null && manga.malId > 0) {
-                            try {
-                                withTimeoutOrNull(2500) {
-                                    val filters = activeSource.getFilterList()
-                                    val searchPage = activeSource.getSearchManga(1, manga.title, filters)
-                                    val topMatch = searchPage.mangas.firstOrNull()
-
-                                    if (topMatch != null) {
-                                        matchedSourceId = activeSource.id
-                                        matchedMangaUrl = topMatch.url
-                                    }
-                                }
-                                delay(300)
-                            } catch (e: Exception) {
-                                // Skip
-                            }
-                        }
-                        manga.copy(
-                            sourceId = matchedSourceId,
-                            mangaUrl = matchedMangaUrl,
-                        )
-                    }
-
-                    DiscoveryProgressState.update(
-                        isRunning = true,
-                        percentage = 90,
-                        message = "Saving to database...",
-                    )
-                    malRepository.insertSeasonalManga(matchedMangaList)
-                }
-            }
-        } catch (e: Exception) {
-            // Skip
-        }
-
-        DiscoveryProgressState.update(
-            isRunning = true,
-            percentage = 100,
-            message = "Done!",
-        )
-        delay(800)
+        DiscoveryProgressState.update(true, 100, "Done!")
+        delay(1200)
         DiscoveryProgressState.reset()
     }
 }
