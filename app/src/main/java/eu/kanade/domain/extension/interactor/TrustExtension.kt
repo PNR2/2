9c230c2aae8bcebb3f0b13d3f3c6a3038b70c6c5
PNR2 <94699808+PNR2@users.noreply.github.com>
2026-08-29 10:53:42 +0530

    Update TrustExtension.kt

--- app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
@@ -3,15 +3,8 @@
 package eu.kanade.domain.extension.interactor
 
 import android.content.pm.PackageInfo
-import dagger.assisted.AssistedInject
-import eu.kanade.domain.source.service.SourcePreferences
-import mihon.domain.extensionrepo.repository.ExtensionRepoRepository
-import tachiyomi.core.common.preference.getAndSet
 
-class TrustExtension @AssistedInject constructor(
-    private val extensionRepoRepository: ExtensionRepoRepository,
-    private val sourcePreferences: SourcePreferences,
-) {
+class TrustExtension {
 
     fun isTrusted(pkgInfo: PackageInfo, signatures: List<String>): Boolean {
         return true
@@ -22,16 +15,10 @@ class TrustExtension @AssistedInject constructor(
     }
 
     fun trust(pkgName: String, versionCode: Long, signatureHash: String) {
-        // mark as trusted in preferences (prevents popup)
-        val trustedSignatures = sourcePreferences.trustedSignatures().get()
-        if (!trustedSignatures.contains(signatureHash)) {
-            sourcePreferences.trustedSignatures().getAndSet {
-                it + signatureHash
-            }
-        }
+        // no-op
     }
 
     fun revokeAll() {
-        // do nothing
+        // no-op
     }
 }
